1face5c330092c153cd2c462da5366131f65e988
Guillermo Legarda <glegardah@gmail.com>
2021-01-19 16:44:38 +0100

    fix spaces/tabs

--- hider_QL.py
+++ hider_QL.py
@@ -33,7 +33,7 @@ from datetime import datetime
 
 ##########################
 #                        #
-#     Set up client      #
+#    Set up client       #
 #                        #
 ##########################
 
@@ -56,25 +56,25 @@ robot = Robot()
 # Define action space
 def stop():
 	'''Do not move'''
-    robot.stop()
+	robot.stop()
 
 def forward():
 	'''Take a step forward'''
-    robot.forward(0.1)
-    time.sleep(0.5)
-    robot.stop()
+	robot.forward(0.1)
+	time.sleep(0.5)
+	robot.stop()
 
 def left():
 	'''Rotate anticlockwise by a fixed amout'''
-    robot.left(0.1)
-    time.sleep(0.5)
-    robot.stop()
+	robot.left(0.1)
+	time.sleep(0.5)
+	robot.stop()
 
 def right():
 	'''Rotate clockwise by a fixed amout'''
-    robot.right(0.1)
-    time.sleep(0.5)
-    robot.stop()
+	robot.right(0.1)
+	time.sleep(0.5)
+	robot.stop()
 
 action_space = [stop, forward, left, right]
 n_actions = len(action_space)
@@ -101,16 +101,16 @@ normalize = torchvision.transforms.Normalize(mean, stdev)
 
 def preprocess(camera_value):
 	'''Transforms frame from the camera for posterior processing'''
-    global device, normalize
-    x = camera_value
-    x = cv2.resize(x, (224, 224))
-    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
-    x = x.transpose((2, 0, 1))
-    x = torch.from_numpy(x).float()
-    x = normalize(x)
-    x = x.to(device)
-    x = x[None, ...]
-    return x
+	global device, normalize
+	x = camera_value
+	x = cv2.resize(x, (224, 224))
+	x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+	x = x.transpose((2, 0, 1))
+	x = torch.from_numpy(x).float()
+	x = normalize(x)
+	x = x.to(device)
+	x = x[None, ...]
+	return x
 
 
 # Object detection
@@ -120,95 +120,95 @@ model = ObjectDetector('ssd_mobilenet_v2_coco.engine')
 camera = Camera.instance(width=300, height=300)
 
 def detection_center(detection):
-    '''Computes the center x, y coordinates of the object'''
-    bbox = detection['bbox']
-    center_x = (bbox[0] + bbox[2]) / 2.0 - 0.5
-    center_y = (bbox[1] + bbox[3]) / 2.0 - 0.5
-    return (center_x, center_y)
+	'''Computes the center x, y coordinates of the object'''
+	bbox = detection['bbox']
+	center_x = (bbox[0] + bbox[2]) / 2.0 - 0.5
+	center_y = (bbox[1] + bbox[3]) / 2.0 - 0.5
+	return (center_x, center_y)
 
 def detect_jetbot(image):
-    '''Detects another jetbot and computes its center'''
-    detections = model(image)
-    # SET CORRECT LABEL VALUE IN NEXT LINE ==================================================
-    matching_detections = [d for d in detections[0] if d['label'] == int(label_widget.value)]
-    # =======================================================================================
-    det = matching_detections[0]
-    center = None
-    if det is not None:
-        center = detection_center(det)
-    return center
+	'''Detects another jetbot and computes its center'''
+	detections = model(image)
+	# SET CORRECT LABEL VALUE IN NEXT LINE ==================================================
+	matching_detections = [d for d in detections[0] if d['label'] == int(label_widget.value)]
+	# =======================================================================================
+	det = matching_detections[0]
+	center = None
+	if det is not None:
+		center = detection_center(det)
+	return center
 
 
 # State observation
 def update_state(): 
-    '''Computes the 3 state components from received data'''
-    # State with 3 components:
-    #  - Horizontal position of detected seeker jetbot
-    #  - Seen/not seen by seeker
-    #  - Free/blocked
-    state_list = [None] * 3
-    
-    # Detect seeker jetbot
-    image = camera.value
-    center = detect_jetbot(image)
-    
-    # Set seeker position
-    if center is not None:
-        state_list[0] = round(center[0])
-    else:
-        state_list[0] = None
-    
-    # Process signal from server and set detection status
-    while True:
-        data = connection.recv(8).decode()
-        if data == '1':
-            state_list[1] = 1
-        else:
-            state_list[1] = 0
-    
-    # Execute collision model to determine if blocked
-    collision_output = collision_model(preprocess(image)).detach().cpu()
-    prob_blocked = float(F.softmax(collision_output.flatten(), dim=1)[0])
-    if prob_blocked > 0.5:
-        state_list[2] = 1
-    else:
-        state_list[2] = 0
-    
-    # Encode state
-    state = state_list[0] + (state_list[1]*2**0 + state_list[2]*2**1)*301
-    
-    return state_list, state
+	'''Computes the 3 state components from received data'''
+	# State with 3 components:
+	#  - Horizontal position of detected seeker jetbot
+	#  - Seen/not seen by seeker
+	#  - Free/blocked
+	state_list = [None] * 3
+	
+	# Detect seeker jetbot
+	image = camera.value
+	center = detect_jetbot(image)
+	
+	# Set seeker position
+	if center is not None:
+		state_list[0] = round(center[0])
+	else:
+		state_list[0] = None
+	
+	# Process signal from server and set detection status
+	while True:
+		data = connection.recv(8).decode()
+		if data == '1':
+			state_list[1] = 1
+		else:
+			state_list[1] = 0
+	
+	# Execute collision model to determine if blocked
+	collision_output = collision_model(preprocess(image)).detach().cpu()
+	prob_blocked = float(F.softmax(collision_output.flatten(), dim=1)[0])
+	if prob_blocked > 0.5:
+		state_list[2] = 1
+	else:
+		state_list[2] = 0
+	
+	# Encode state
+	state = state_list[0] + (state_list[1]*2**0 + state_list[2]*2**1)*301
+	
+	return state_list, state
 
 
 # Define reward function
 def compute_reward(action, state_list):
 	'''Calculate the reward obtained by taking action from state_list'''
-    # Penalise being seen
-    if state_list[1] == 1:
-        reward = -1
-    else:
-        reward = 1
-    
-    # Penalise collisions
-    if state_list[2] == 1 && action == 1:
-        reward = -50
+	# Penalise being seen
+	if state_list[1] == 1:
+		reward = -1
+	else:
+		reward = 1
+	
+	# Penalise collisions
+	if state_list[2] == 1 && action == 1:
+		reward = -50
 
-    return reward
+	return reward
 
 
 # Define environment
 def step(action):
-    '''Executes an action, observes the state and computes the reward'''
-    # Execute action
-    action_space[action]()
-    
-    # Observe state
-    state_list, state = update_state()
-    
-    # Compute reward
-    reward = compute_reward(action, state_list)
-    
-    return state, reward
+	'''Executes an action, observes the state and computes the reward'''
+	# Execute action
+	action_space[action]()
+	
+	# Observe state
+	state_list, state = update_state()
+	
+	# Compute reward
+	reward = compute_reward(action, state_list)
+	
+	return state, reward
 
 
 ##########################
@@ -226,13 +226,13 @@ q_table = np.zeros((n_states, n_actions))
 #                        #
 ##########################
 
-n_train_episodes = 100     # number of rollouts for training
-max_steps = 240            # maximum number of steps per rollout
+n_train_episodes = 100    # number of rollouts for training
+max_steps = 240           # maximum number of steps per rollout
 
-gamma = 0.9                # discout factor
-alpha = 0.6                # learning rate
+gamma = 0.9               # discout factor
+alpha = 0.6               # learning rate
 
-epsilon = 1.0              # exploration-exploitation trade-off
+epsilon = 1.0             # exploration-exploitation trade-off
 max_epsilon = 1.0
 min_epsilon = 0.01
 decay_rate = 0.01
@@ -253,49 +253,49 @@ socket_hider.sendall('HIDER ready').encode()
 data = socket_hider.recv(8)
 
 for episode in range(n_train_episodes):
-    # Reset stats
-    episode_start = time.time()
-    reward_episode = 0
-    
-    # Reset the environment
-    state = update_state()
-    step = 0
-    message = '0'
+	# Reset stats
+	episode_start = time.time()
+	reward_episode = 0
+	
+	# Reset the environment
+	state = update_state()
+	step = 0
+	message = '0'
 
-    for iteration in range(max_steps):
-        # Choose action according to exploration-exploitation tradeoff
-        tradeoff = random.uniform(0, 1)
-        if tradeoff > epsilon: # exploitation (on-policy)
-            action = np.argmax(q_table[state, :])
-        else: # exploration (off-policy)
-            action = np.random.randint(n_actions)
+	for iteration in range(max_steps):
+		# Choose action according to exploration-exploitation tradeoff
+		tradeoff = random.uniform(0, 1)
+		if tradeoff > epsilon: # exploitation (on-policy)
+			action = np.argmax(q_table[state, :])
+		else: # exploration (off-policy)
+			action = np.random.randint(n_actions)
 
-        # Take action and observe new state and reward
-        new_state, reward = step(action)
-        reward_episode += reward
+		# Take action and observe new state and reward
+		new_state, reward = step(action)
+		reward_episode += reward
 
-        # Update Q-table
-        q_table[state, action] = q_table[state, action] + alpha*(reward + gamma*np.amax(q_table[new_state, :])
-                                                                 - q_table[state, action])
+		# Update Q-table
+		q_table[state, action] = q_table[state, action] + alpha*(reward + gamma*np.amax(q_table[new_state, :])
+																 - q_table[state, action])
 
-        # Update state
-        state = new_state
-        
-        # Send status to server
-        if iteration == max_steps:
-            message = '1'
-        socket_hider.sendall(message.encode())
+		# Update state
+		state = new_state
+		
+		# Send status to server
+		if iteration == max_steps:
+			message = '1'
+		socket_hider.sendall(message.encode())
 
-    # Reduce epsilon
-    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
-    
-    # Log training stats
-    rewards_training[episode] = reward_episode
-    episode_stop = time.time()
-    timing_training[episode] = episode_stop - episode_start
-    
-    # Toggle new episode
-    data = socket_hider.recv(8)
+	# Reduce epsilon
+	epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
+	
+	# Log training stats
+	rewards_training[episode] = reward_episode
+	episode_stop = time.time()
+	timing_training[episode] = episode_stop - episode_start
+	
+	# Toggle new episode
+	data = socket_hider.recv(8)
 
 
 ##########################
@@ -307,10 +307,10 @@ for episode in range(n_train_episodes):
 # Create timestamped directory to save data
 new_dir = os.path.join(os.getcwd(), datetime.now().strftime('%d-%m-%Y_%H-%M-%S'))
 try:
-    os.makedirs(new_dir)
+	os.makedirs(new_dir)
 except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise  # This was not a "directory exist" error.
+	if e.errno != errno.EEXIST:
+		raise  # This was not a "directory exist" error.
 
 # Save data
 np.savetxt(new_dir+'/Q_table_H', q_table, delimiter=',')
--- jetbot_server.py
+++ jetbot_server.py
@@ -10,97 +10,97 @@ import sys
 import threading
 
 class Server:
-    def __init__(self):
-        self.clients = []             # List of connected clients: [seeker, hider]
-        self.ip = 'localhost'         # Server IP address
-        self.port = 10000             # Server port number
-        self.buffer_size = 16         # Buffer size
-        self.hider_ready = False      # True if hider is ready to begin training
-        self.seeker_ready = False     # True if seeker is ready to begin training
-        self.begin_training = False   # True to toggle the beginning of the training process
-        self.episode_ended = False    # True if the hider has finished the current episode
-        self.detection_set = False    # True if seeker data has been processed
-        self.detected = '0'           # Detection status (1: visual contact; 0: no visual contact)
-        self.ping = '1'               # Arbitrary small data packet
+	def __init__(self):
+		self.clients = []             # List of connected clients: [seeker, hider]
+		self.ip = 'localhost'         # Server IP address
+		self.port = 10000             # Server port number
+		self.buffer_size = 16         # Buffer size
+		self.hider_ready = False      # True if hider is ready to begin training
+		self.seeker_ready = False     # True if seeker is ready to begin training
+		self.begin_training = False   # True to toggle the beginning of the training process
+		self.episode_ended = False    # True if the hider has finished the current episode
+		self.detection_set = False    # True if seeker data has been processed
+		self.detected = '0'           # Detection status (1: visual contact; 0: no visual contact)
+		self.ping = '1'               # Arbitrary small data packet
 
-    def start_server(self):
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.bind((self.ip, self.port))
-            sock.listen(5)
-            while True:
-                client_socket, addr = sock.accept()
-                self.clients.append(client_socket)
-                if len(self.clients) == 1:
-                    print ('Connected with ' + addr[0] + ':' + str(addr[1]) + '--> SEEKER')
-                    threading.Thread(target=self.threaded_seeker, args=(client_socket,)).start()
-                elif len(self.clients) == 2:
-                    print ('Connected with ' + addr[0] + ':' + str(addr[1]) + '--> HIDER')
-                    threading.Thread(target=self.threaded_hider, args=(client_socket,)).start()
-            sock.close()
-        except socket.error as msg:
-            print ('Could not start SERVER thread.')
-            sys.exit()
-    
-    def threaded_seeker(self, client_socket):
-        # Wait until both hider and seeker are ready to begin training
-        data = client_socket.recv(self.buffer_size).decode()
-        if data:
-            print(data)
-            self.seeker_ready = True
-            while not self.hider_ready:
-                pass
-            input("Press ENTER to begin training:")
-            self.begin_training = True
-        # Training loop
-        while self.begin_training:
-            # Receive and process detection status
-            data = client_socket.recv(self.buffer_size).decode()
-            if data:
-                self.detected = data
-                self.detection_set = True
-            else:
-                break
-            # Receive episode status and toggle the next one
-            data = client_socket.recv(self.buffer_size).decode()
-            if data:
-                if data == '1':
-                    while not self.episode_ended:
-                        pass
-                    input("Episode done. Press ENTER to begin next episode:")
-                    for client in self.clients:
-                        client.send(ping.encode())
-                    self.episode_ended = False
-            else:
-                break
-        self.clients.remove(client_socket)
-        client_socket.close()
-        
-    def threaded_hider(self, client_socket):
-        # Wait for hider to be ready to begin training
-        data = client_socket.recv(self.buffer_size).decode()
-        if data:
-            print(data)
-            self.hider_ready = True
-        # Wait for user to toggle the beginning of the training process
-        while not self.begin_training:
-            pass
-        # Training loop
-        while self.begin_training:
-            # Wait for seeker and send detection status
-            while not self.detection_set:
-                pass
-            client_socket.send(self.detected.encode())
-            self.detection_set = False
-            # Receive and process episode status
-            data = client_socket.recv(self.buffer_size).decode()
-            if data:
-                if data == '1':
-                    self.episode_ended = True
-            else:
-                break
-        self.clients.remove(client_socket)
-        client_socket.close()
+	def start_server(self):
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.bind((self.ip, self.port))
+			sock.listen(5)
+			while True:
+				client_socket, addr = sock.accept()
+				self.clients.append(client_socket)
+				if len(self.clients) == 1:
+					print ('Connected with ' + addr[0] + ':' + str(addr[1]) + '--> SEEKER')
+					threading.Thread(target=self.threaded_seeker, args=(client_socket,)).start()
+				elif len(self.clients) == 2:
+					print ('Connected with ' + addr[0] + ':' + str(addr[1]) + '--> HIDER')
+					threading.Thread(target=self.threaded_hider, args=(client_socket,)).start()
+			sock.close()
+		except socket.error as msg:
+			print ('Could not start SERVER thread.')
+			sys.exit()
+	
+	def threaded_seeker(self, client_socket):
+		# Wait until both hider and seeker are ready to begin training
+		data = client_socket.recv(self.buffer_size).decode()
+		if data:
+			print(data)
+			self.seeker_ready = True
+			while not self.hider_ready:
+				pass
+			input("Press ENTER to begin training:")
+			self.begin_training = True
+		# Training loop
+		while self.begin_training:
+			# Receive and process detection status
+			data = client_socket.recv(self.buffer_size).decode()
+			if data:
+				self.detected = data
+				self.detection_set = True
+			else:
+				break
+			# Receive episode status and toggle the next one
+			data = client_socket.recv(self.buffer_size).decode()
+			if data:
+				if data == '1':
+					while not self.episode_ended:
+						pass
+					input("Episode done. Press ENTER to begin next episode:")
+					for client in self.clients:
+						client.send(ping.encode())
+					self.episode_ended = False
+			else:
+				break
+		self.clients.remove(client_socket)
+		client_socket.close()
+		
+	def threaded_hider(self, client_socket):
+		# Wait for hider to be ready to begin training
+		data = client_socket.recv(self.buffer_size).decode()
+		if data:
+			print(data)
+			self.hider_ready = True
+		# Wait for user to toggle the beginning of the training process
+		while not self.begin_training:
+			pass
+		# Training loop
+		while self.begin_training:
+			# Wait for seeker and send detection status
+			while not self.detection_set:
+				pass
+			client_socket.send(self.detected.encode())
+			self.detection_set = False
+			# Receive and process episode status
+			data = client_socket.recv(self.buffer_size).decode()
+			if data:
+				if data == '1':
+					self.episode_ended = True
+			else:
+				break
+		self.clients.remove(client_socket)
+		client_socket.close()
 
 # Start server
 server = Server()
